7b5aafd892cfa9002a78a6a61df303f5f79ef8a2
shadrack <castroshadrack7@gmail.com>
2024-10-17 23:42:18 +0800

    initial commit

---
+++ nav.js
@@ -0,0 +1,13 @@
+const nav = document.querySelector('.navbar')
+
+document.querySelector('.burger')
+.addEventListener('click', () => {
+    nav.classList.remove('hidden')
+    nav.classList.add('smallNav')
+})
+
+document.querySelector('.burger-close')
+.addEventListener('click', () => {
+    nav.classList.add('hidden')
+    nav.classList.remove('smallNav')
+})
