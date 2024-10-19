2ba1d70b0fb2b80dab3b6067a961c07184ec94c8
shadrack <castroshadrack7@gmail.com>
2024-10-19 17:44:45 +0800

    make a folder for counter functions., add comments for readability.

---
+++ scripts/mainPage/counter.js
@@ -0,0 +1,19 @@
+export function incrementQuantity() {
+    const productId = button.dataset.productId
+    productQuantities[productId] += 1
+    let prodQuantityHTML = document.querySelector(`.js-quantity-${productId}`)
+    prodQuantityHTML.innerHTML = productQuantities[productId]
+}
+
+export function decrementQuantity() {
+    const productId = button.dataset.productId
+    let prodQuantityHTML = document.querySelector(`.js-quantity-${productId}`)
+
+    if(productQuantities[productId] <= 1) {
+      alert('error')
+    } else {
+      productQuantities[productId] -= 1
+    
+      prodQuantityHTML.innerHTML = productQuantities[productId]
+    }
+}
