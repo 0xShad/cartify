641bd42a4391e1b4fb18c4efa3d73e9aed5f13d7
shadrack <castroshadrack7@gmail.com>
2024-10-19 19:33:38 +0800

    fixed counter buttons

--- scripts/mainPage/counter.js
+++ scripts/mainPage/counter.js
@@ -1,11 +1,11 @@
-export function incrementQuantity() {
+export function incrementQuantity(button, productQuantities) {
     const productId = button.dataset.productId
     productQuantities[productId] += 1
     let prodQuantityHTML = document.querySelector(`.js-quantity-${productId}`)
     prodQuantityHTML.innerHTML = productQuantities[productId]
 }
 
-export function decrementQuantity() {
+export function decrementQuantity(button, productQuantities) {
     const productId = button.dataset.productId
     let prodQuantityHTML = document.querySelector(`.js-quantity-${productId}`)
 
